fcb010a28067e67085090406a63e969e654d7b2b
MarcusRei <marcusreineck@gmail.com>
2022-11-28 13:21:06 +0100

    first commit

---
+++ src/ts/movieApp.ts
@@ -0,0 +1,63 @@
+import { IMovie } from "./models/Movie";
+import { getData } from "./services/movieservice";
+//Skapar lista
+let movies: IMovie[] = [];
+
+//Lägger till submithändelselyssnare på searchform
+export const init = () => {
+  let form = document.getElementById("searchForm") as HTMLFormElement;
+  form.addEventListener("submit", (e: SubmitEvent) => {
+    e.preventDefault();
+    handleSubmit();
+  });
+};
+
+export async function handleSubmit() {
+  let searchText = (document.getElementById("searchText") as HTMLInputElement)
+    .value;
+  //Tömmer movie-containern
+  let container: HTMLDivElement = document.getElementById(
+    "movie-container"
+  ) as HTMLDivElement;
+  container.innerHTML = "";
+  //Ny grej??
+  try {
+    //Sätter datan i listan från tidigare med funktionen från services
+    movies = await getData(searchText);
+
+    if (movies.length > 0) {
+      exports.createHtml(movies, container);
+    } else {
+      exports.displayNoResult(container);
+    }
+  } catch {
+    exports.displayNoResult(container);
+  }
+}
+//Skapar upp HTML
+export const createHtml = (movies: IMovie[], container: HTMLDivElement) => {
+  for (let i = 0; i < movies.length; i++) {
+    let movie = document.createElement("div");
+    let title = document.createElement("h3");
+    let img = document.createElement("img");
+
+    //Sätter poster och title i repsektive html-element, samt lägger på class "movie"
+    movie.classList.add("movie");
+    title.innerHTML = movies[i].Title;
+    img.src = movies[i].Poster;
+    img.alt = movies[i].Title;
+
+    movie.appendChild(title);
+    movie.appendChild(img);
+
+    container.appendChild(movie);
+  }
+};
+//Visar ett tomt resultat om inga sökreusltat
+export const displayNoResult = (container: HTMLDivElement) => {
+  let noMessage = document.createElement("p");
+
+  noMessage.innerHTML = "Inga sökresultat att visa";
+
+  container.appendChild(noMessage);
+};
